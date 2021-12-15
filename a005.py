c6f847c893fd015d40251f690b8d4b100ac9902c
phyop <seek1andfind2@gmail.com>
2021-12-15 09:04:07 +0800

    3rd

--- a005.py
+++ a005.py
@@ -3,7 +3,7 @@ for i in range(t):
     # https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/
     l = list(map(int,input("\nseries : ").strip().split()))
     d = l[3]-l[2]
-    m = l[3]/l[2]
+    m = l[3]//l[2]
     # 如果是等差，那越後面的相鄰數值，越不可能整除，所以藉由判別是不是整除，來確定等比還是等差
     # 最小的等差 1234
     # 最小的等比 1248
