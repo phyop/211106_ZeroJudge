08babffa49bf3216401b330bb38a2e0802007e74
phyop <seek1andfind2@gmail.com>
2021-12-19 17:54:41 +0800

    質數表成功！但把所有數先append進list再remove的做法會TLE(Time Limit Exceeded)。

--- a010.py
+++ a010.py
@@ -1,30 +1,34 @@
 import math
 
-cin = int(input("Num: "))
-# 質數範圍：2~sqrt(x) 
+cin = 1000000
+# cin = int(input("Num: "))
+# 質數範圍：2~sqrt(cin) 
 ls = [2]
 mid = int(math.sqrt(cin))
-# 建構質數表
+# 建構質數範圍列表
 for i in range(3, mid):
     ls.append(i)
-print(ls)
-# 刪除第1個質數的倍數
-for i in ls:
-    if i % ls[0] == 0:
-        ls.remove(i)
 # 找第一個未被刪掉的數字n=ls[0]，刪除其2~mid//n倍
-for i in range(2,mid//ls[0]):
-    ls.remove(ls[0]*i)
+# 找下一個數字ls[1]，刪除其倍數，當ls[0]*2>mid就可以停止
+order_of_prime = 0
+while ls[order_of_prime]*2 < mid:
+    for i in range(2,mid//ls[0]+1):
+        if ls[order_of_prime]*i in ls:
+            ls.remove(ls[order_of_prime]*i)  
+    order_of_prime += 1
+print(len(ls))
+# 1000以內的質數的確有168個
 
-print(ls)
+# while True:
+#     try:
+#         cin_real = int(input())
+#         if cin_real in ls:
+#             print("質數")
+#         else:
+#             print("非質數")
+#     except:
+#         break
 
-
-
-# 刪掉 2 的倍數。
-# 找下一個未被刪掉的數字，
-# 找到 3 ，
-# 刪掉 3 的倍數。
-# 找下一個未被刪掉的數字。
 # //判斷要不要印出”*”， 次方=1就不用印"^"了
 # print(str(s)+'^'+str(n),end='') 輸出時記得設print()為不要換行
 
