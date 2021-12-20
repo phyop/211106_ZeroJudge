08cbb8da422f9b05a40b8e6d480f01cf00d37ca2
phyop <seek1andfind2@gmail.com>
2021-12-20 08:33:16 +0800

    質數範圍是2~cin，分解範圍才是2~sqrt(cin)

--- a010.py
+++ a010.py
@@ -1,27 +1,45 @@
 import math
 
 # cin = 100000000
-cin = int(input())
-# 質數範圍：2~sqrt(cin) 
+cin = int(input("num: "))
+# 質數範圍：2~cin
 ls = [2]
-mid = int(math.sqrt(cin))
 # 建構質數範圍列表
-for i in range(3, mid):
+for i in range(3, cin+1):
     ls.append(i)
-# 找第一個未被刪掉的數字n=ls[0]，刪除其2~mid//n倍
-# 找下一個數字ls[1]，刪除其倍數，當ls[0]*2>mid就可以停止
+# 找第一個未被刪掉的數字n=ls[0]，刪除其2~cin//n倍
+# 找下一個數字ls[1]，刪除其倍數，當ls[0]*2>cin就可以停止
 order_of_prime = 0
-while ls[order_of_prime]*2 < mid:
-    for i in range(2,mid//ls[0]+1):
+while ls[order_of_prime]*2 < cin:
+    for i in range(2,cin//ls[0]+1):
         if ls[order_of_prime]*i in ls:
             ls.remove(ls[order_of_prime]*i)  
     order_of_prime += 1
 
-# 對cin質因數分解
-
-
+# 找cin質因數
+factor = []
+for i in range(len(ls)):
+    if cin//ls[i] == 0:
+        factor.append(i)
+# 質因數的次方
+for i in range(len(factor)):
+    power = 0
+    while cin//factor[i] ==0:
+        power += 1
+    if power != 0:
+        print(str(ls[i])+'^'+str(power),end='')
+    else:
+        print(str(s),end='')
+    if i+1 != len(factor):
+        print(" * ",end='')
+print()
 # //判斷要不要印出”*”， 次方=1就不用印"^"了
-# print(str(s)+'^'+str(n),end='') 輸出時記得設print()為不要換行
+
 # 20 -> 2^2 * 5
 # 17 -> 17
 # 999997 -> 757 * 1321
+
+# a1 = 2
+# a2 = 5
+# p1 =2
+# print("%d^%d * %d" %(a1,p1,a2))
