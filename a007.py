12835f83a25987d82eac2423b101122e29012f8c
phyop <seek1andfind2@gmail.com>
2021-12-23 21:52:32 +0800

    ok

--- a007.py
+++ a007.py
@@ -1,23 +1,19 @@
 import math
 
-# cin = 2147483647
-cin = 67483647
-# 質數範圍：2~sqrt(cin) 
-ls = [2]
-mid = int(math.sqrt(cin))
-# 建構質數範圍列表
-for i in range(3, mid):
+cin = 10000
+ls = []
+# 建構質數範圍列表，質數範圍：2~cin
+for i in range(2, cin+1):
     ls.append(i)
-# 找第一個未被刪掉的數字n=ls[0]，刪除其2~mid//n倍
-# 找下一個數字ls[1]，刪除其倍數，當ls[0]*2>mid就可以停止
+# 找第一個未被刪掉的數字n=ls[0]，刪除其倍數，i.e 2~cin//n倍
+# 找下一個數字ls[0 + count]，刪除其倍數，i.e 2~cin//ls[0 + count]倍
+# 直到ls[-1]
 order_of_prime = 0
-while ls[order_of_prime]*2 < mid:
-    for i in range(2,mid//ls[0]+1):
-        if ls[order_of_prime]*i in ls:
+while ls[order_of_prime] != ls[-1]: # 直到ls[-1]
+    for i in range(2,cin//ls[order_of_prime]+1): # 2~cin//n倍
+        if ls[order_of_prime]*i in ls: # 刪除其倍數
             ls.remove(ls[order_of_prime]*i)  
-    order_of_prime += 1
-# print(len(ls))
-# 1000以內的質數有168個
+    order_of_prime += 1 
 
 while True:
     try:
--- a010.py
+++ a010.py
@@ -1,37 +1,40 @@
 import math
 
-# cin = 100000000
-cin = int(input("num: "))
-# 質數範圍：2~cin
-ls = [2]
-# 建構質數範圍列表
-for i in range(3, cin+1):
+cin = 10000
+ls = []
+# 建構質數範圍列表，質數範圍：2~cin
+for i in range(2, cin+1):
     ls.append(i)
-# 找第一個未被刪掉的數字n=ls[0]，刪除其2~cin//n倍
-# 找下一個數字ls[1]，刪除其倍數，當ls[0]*2>cin就可以停止
+# 找第一個未被刪掉的數字n=ls[0]，刪除其倍數，i.e 2~cin//n倍
+# 找下一個數字ls[0 + count]，刪除其倍數，i.e 2~cin//ls[0 + count]倍
+# 直到ls[-1]
 order_of_prime = 0
-while ls[order_of_prime]*2 < cin:
-    for i in range(2,cin//ls[0]+1):
-        if ls[order_of_prime]*i in ls:
+while ls[order_of_prime] != ls[-1]: # 直到ls[-1]
+    for i in range(2,cin//ls[order_of_prime]+1): # 2~cin//n倍
+        if ls[order_of_prime]*i in ls: # 刪除其倍數
             ls.remove(ls[order_of_prime]*i)  
-    order_of_prime += 1
+    order_of_prime += 1 
 
+cin_real = int(input())
 # 找cin質因數
 factor = []
 for i in range(len(ls)):
-    if cin//ls[i] == 0:
-        factor.append(i)
+    if cin_real % ls[i] == 0:
+        factor.append(ls[i])
 # 質因數的次方
 for i in range(len(factor)):
     power = 0
-    while cin//factor[i] ==0:
+    while cin_real % factor[i] == 0:
+        cin_real //= factor[i]
         power += 1
-    if power != 0:
-        print(str(ls[i])+'^'+str(power),end='')
+    if power = 0:
+        continue
+    elif power = 1:
+        print(str(ls[i]),end='')
     else:
-        print(str(s),end='')
+        print(str(ls[i])+'^'+str(power),end='')
     if i+1 != len(factor):
-        print(" * ",end='')
+        print(' * ')
 print()
 # //判斷要不要印出”*”， 次方=1就不用印"^"了
 
