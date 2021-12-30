0de30b50687c4c3f567a8f71c4fa074c6003e3c1
phyop <seek1andfind2@gmail.com>
2021-12-30 22:22:38 +0800

    PASS! 2021-12-30 4 14:21:44 PM 多筆測資，所以要用While True

--- a015_矩陣翻轉_no numpy.py
+++ a015_矩陣翻轉_no numpy.py
@@ -1,16 +1,40 @@
-m, n = map(int, input().strip().split())
-row = []
+while True:
+    try:
+        ori_array = []
+        flip_array = []
+        ori_r, ori_c = map(int, input().strip().split())
+        flip_r = ori_c
+        flip_c = ori_r
+        
+        for _ in range(ori_r):
+            # 每一row就建一個list
+            ori_ri = list(map(int, input().strip().split()))
+            # 用一個list，把每一個row都裝進來，就是array了
+            ori_array.append(ori_ri)
+        
+        for i in range(flip_r):
+            # flip有幾row，就先建幾個空list
+            flip_array.append([])
+            for j in range(flip_c):
+                # 先對第i個row，把值填完
+                flip_array[i].append(ori_array[j][i])
+        
+        for r in range(flip_r):
+            # *就是解包的意思
+            print(*flip_array[r])
+    except:
+        break
+
+# for r in range(flip_r):
+#     if r!= 0:
+#         print()
+#     for c in range(flip_c):
+#         # 不知道為什麼，加了「end=」，在某些compiler輸出就啥都沒有，或最後一行沒有
+#         print(flip_array[r][c], end=' ')
+#         # from sys import stdout
+#         # stdout.write(flip_array[r][c]+' ')
+
 # 3 4
 # 3 5 6 7
 # 1 3 4 66
 # 3 5 6 77
-
-for i in range(m):
-    r = list(map(int, input().strip().split()))
-    row.append(r)
-# [[3, 5, 6, 7], [1, 3, 4, 66], [3, 5, 6, 77]]
-
-zero_arr = np.zeros((n,m))
-for i in range(n):
-    for j in range(m):
-        zero_arr[i][j] = array[j][i]
