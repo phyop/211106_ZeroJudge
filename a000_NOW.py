d672f465aa5eb4e03dd08a73bd5bbdff28350ab6
phyop <seek1andfind2@gmail.com>
2022-01-05 06:14:43 +0800

    add notes of vi operations

--- a000_NOW.py
+++ a000_NOW.py
@@ -5,3 +5,18 @@ print(result)
 
 # sys.stdout.write就不會換行
 # sympy在解跟質數相關的題目真的很好用 :D
+
+"""
+https://knowledge.chinesewords.org/319502-67.html
+ggVG vim全選 
+gg 讓光標移到首行，在vim才有效，vi中無效
+V 是進入Visual(可視）模式
+G 光標移到最後一行
+
+:1,$y 全部復制
+dG 刪除光標所在行到最後一行的內容（包括光標所在行的內容）
+
+d 刪除選中內容
+y 復制選中內容到0號寄存器
+"+y 復制選中內容到＋寄存器，也就是系統的剪貼板
+"""
