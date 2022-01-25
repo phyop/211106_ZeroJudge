251eb2e981b22664691ec4daa54735ba33cba21d
phyop <seek1andfind2@gmail.com>
2022-01-25 16:17:16 +0800

    pass!

--- 220125_coursera_c-bian-cheng_surAndVol.py
+++ 220125_coursera_c-bian-cheng_surAndVol.py
@@ -5,15 +5,36 @@
 # w = int(input("寬度w = "))
 # d = int(input("長度d = "))
 
-path = "./"
-f_in = open(path_in, "r")
-lines = f_in.readlines()
-for line in lines:
-    pass
-f_in.close()
+path = "./in.txt"
+hwd = []
 
+f = open(path, "r")
+lines = f.readlines()
+for line in lines:
+    hwd.append(int(line))
+f.close()
+
+h, w, d = hwd[0], hwd[1], hwd[2]
 sur = 2*(h*w + w*d + d*h)
 vol = h*w*d
 
 print(sur, vol, end=" ")
 print()
+
+"""
+#include <stdio.h>
+
+int main ()
+{
+	int h, w, d;
+	int surface_area, volume;
+
+	scanf ( "%d%d%d", &h, &w, &d );
+
+	surface_area	= 2 * ( h * w + w * d + d * h );
+	volume 		= h * w * d;
+
+	printf ( "%d %d", surface_area, volume );
+	return 0;
+}
+"""
